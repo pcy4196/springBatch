67b9c201ec53608827d234a72aefbea282451f5d
PCY <pcy4196@naver.com>
2022-07-11 19:22:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### springBatch
+***
++ practice springBatch in springBoot
+  + 스프링 배치 소개
+    + Ch01. 스프링 배치 소개
+  + 스프링 배치 시작(START)
+    + Ch02. 프로젝트 구성 및 의존성 설정
+    + Ch03. Hello Spring Batch 시작
+        + HelloJobConfiguration 구현
---
+++ src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
@@ -0,0 +1,60 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor // 생성자 추가 필요 X - LomBok
+@Configuration
+public class HelloJobConfiguration {
+
+    // 의존성 주입
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep1())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return stepBuilderFactory.get("helloStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" ========================");
+                        System.out.println(" ### Hello Spring Batch -- helloStep1 !!");
+                        System.out.println(" ========================");
+                        return RepeatStatus.FINISHED;   // 해당 기능 한번 수행하고 종료
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" ========================");
+                        System.out.println(" ### Hello Spring Batch -- helloStep2 !!");
+                        System.out.println(" ========================");
+                        return RepeatStatus.FINISHED;   // 해당 기능 한번 수행하고 종료
+                    }
+                })
+                .build();
+    }
+}
